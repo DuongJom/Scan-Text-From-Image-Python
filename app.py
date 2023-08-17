600ee48fdf6f4dc714cccde87b4f805733982da1
Nguyentb <ngocduong5642@gmail.com>
2023-08-17 08:12:05 +0700

    extract useful data from image

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from tkinter import Tk, Button, Text, filedialog as fd
 from tkinter import ttk
 from PIL import Image
 from pytesseract import pytesseract
+from prettytable import PrettyTable
 import os
 
 path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
@@ -11,7 +12,7 @@ pytesseract.tesseract_cmd = path_to_tesseract
 class App():
     def __init__(self):
         self.root = Tk()
-        self.root.geometry("600x400")
+        self.root.geometry("590x650")
         self.root.title("Scan Text Tool")
         
         self.btnSelectImage = Button(self.root, text="Select images", command=self.open_select_files_dialog)
@@ -20,8 +21,11 @@ class App():
         self.cbbPath = ttk.Combobox(self.root, values=(), width=50)
         self.cbbPath.place(x=120, y=15)
 
-        self.extractedText = Text(self.root, width=70, height=20)
+        self.extractedText = Text(self.root, width=70, height=15)
         self.extractedText.place(x=10, y=50)
+
+        self.table = Text(self.root, width=70, height=20)
+        self.table.place(x=10, y=300)
 
         self.btnExtract = Button(self.root, text="Extract", command=self.extract_text)
         self.btnExtract.place(x=450,y=10)
@@ -53,6 +57,16 @@ class App():
             self.extractedText.delete(tk.END)
             img = Image.open(self.cbbPath.get())
             self.extractedText.insert(tk.END, pytesseract.image_to_string(img, lang='vie'))
+            content = self.extractedText.get(1.0, tk.END).split('\n')
+            p_table = PrettyTable()
+            p_table.field_names = ["Số hiệu", "Ngày/tháng/năm"]
+            for line in content:
+                so_hieu = line[line.find("số "): line.find(" ngày")]
+                ngay = line[line.find("ngày "): line.find("năm ")+9]
+                if so_hieu != '' and ngay != '':
+                    p_table.add_row([so_hieu.split("số ")[1], ngay])
+            self.table.insert(tk.END, p_table)
+
 
     def export_to_excel(self):
         filetypes = (
